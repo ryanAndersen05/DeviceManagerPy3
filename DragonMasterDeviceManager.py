8ccc703e854c2b8fefee00c87a5fd0e9daa25046
Ryan Andersen <ryan.andersen05@gmail.com>
2019-08-19 18:48:09 -0400

    Adding comments to TCPManager

--- DragonMasterDeviceManager.py
+++ DragonMasterDeviceManager.py
@@ -9,24 +9,34 @@ Our device manager class that will find and hold all of our connected devices an
 It will manages messages between our Unity Application and assign commands to the correct devices.
 """
 class DragonMasterDeviceManager:
-    #General Device Commands
+    ##General Device Commands
     DEVICE_CONNECTED = 0X01
     DEVICE_DISCONNECTED = 0X02
 
-    #DRAX COMMANDS
+    ##DRAX COMMANDS
     DRAX_ID = 0X10
+    #Send Events
     DRAX_INPUT_EVENT = 0X11
+    #Receive Events
+    DRAX_OUTPUT_EVENT = 0X12
+    DRAX_OUTPUT_BIT_EVENT = 0X13
+    DRAX_HARD_METER_EVENT = 0X14
 
-    #JOYSTICK COMMANDS
+    ##JOYSTICK COMMANDS
     JOYSTICK_ID = 0X20
     JOYSTICK_INPUT_EVENT = 0X21
 
 
-    #PRINTER COMMANDS
+    ##PRINTER COMMANDS
     PRINTER_ID = 0X40
 
+    CUSTOM_TG02 = 0X01
+    RELIANCE_PRINTER = 0X02
+    PYRAMID_PRINTER = 0X03
+    
 
-    #BILL ACCEPTOR COMMANDS
+
+    ##BILL ACCEPTOR COMMANDS
     BILL_ACCEPTOR_ID = 0X80
 
 
@@ -45,10 +55,11 @@ class DragonMasterDeviceManager:
     """
     def add_event_to_send(self, eventToSend):
         self.tcpManager.add_event_to_send(eventToSend)
-
         return
 
-
+    """
+    Upon receiving an event from our Unity Application, we will process the command through this method
+    """
     def process_received_event(self, eventReceived):
 
         return
@@ -69,16 +80,23 @@ class TCPManager:
         self.tcpEventQueue = queue.Queue()#Queue of events that we want to send to Unity
 
 
+    """
+    Be sure that the event that is passed through is of the type bytearray
+    """
     def add_event_to_send(self, eventToSend):
         if (eventToSend == None):
             print ("The event added was null.")
             return
         self.tcpEventQueue.put(eventToSend)
-        
+
         if (not self.sendingEventsToOurUnityApplication):
             self.socket_send()
         return
 
+    """
+    This method sends all of the events that are currently in our event queue to our
+    Unity Application
+    """
     def socket_send(self):
         self.sendingEventsToOurUnityApplication = True
 
@@ -87,6 +105,9 @@ class TCPManager:
         self.sendingEventsToOurUnityApplication = False
         return
 
+    """
+    This method will 
+    """
     def socket_receive(self):
 
         return
