97d08da9971217ea83109a30ed91f790e065b563
Ryan Andersen <ryan.andersen05@gmail.com>
2019-08-19 18:26:23 -0400

    Making a TCP Manager class

--- DragonMasterDeviceManager.py
+++ DragonMasterDeviceManager.py
@@ -30,15 +30,64 @@ class DragonMasterDeviceManager:
     BILL_ACCEPTOR_ID = 0X80
 
 
+    #TCP Variables
+
+
 
     def __init__(self,):
-        self.tcpEventQueue = queue.Queue()#Queue of all the events that we plan to send to Unity
+        self.tcpManager = TCPManager()
         return
 
     #TCP Communication ###############################################################
+    """
+    Queue up an event to send to our Unity Application. This should always be of the type
+    byteArray
+    """
+    def add_event_to_send(self, eventToSend):
+        self.tcpManager.add_event_to_send(eventToSend)
+
+        return
 
 
+    def process_received_event(self, eventReceived):
+
+        return
+        
     #END TCP Communication ###########################################################
 
 
+    pass
+
+
+"""
+Class that handles all of our TCP communication
+"""
+class TCPManager:
+
+    def __init__(self):
+        self.sendingEventsToOurUnityApplication = False
+        self.tcpEventQueue = queue.Queue()#Queue of events that we want to send to Unity
+
+
+    def add_event_to_send(self, eventToSend):
+        if (eventToSend == None):
+            print ("The event added was null.")
+            return
+        self.tcpEventQueue.put(eventToSend)
+        
+        if (not self.sendingEventsToOurUnityApplication):
+            self.socket_send()
+        return
+
+    def socket_send(self):
+        self.sendingEventsToOurUnityApplication = True
+
+
+
+        self.sendingEventsToOurUnityApplication = False
+        return
+
+    def socket_receive(self):
+
+        return
     pass
--- DragonMasterSerialDevice.py
+++ DragonMasterSerialDevice.py
@@ -3,6 +3,9 @@ import DragonMasterDeviceManager
 import serial
 
 
+"""
+The base class for all devices that use Serial communication
+"""
 class SerialDevice(DragonMasterDevice.DragonMasterDevice):
 
     def __init__(self, deviceManager,):
@@ -10,6 +13,9 @@ class SerialDevice(DragonMasterDevice.DragonMasterDevice):
         return
     pass
 
+"""
+A class that handles all our Bill Acceptor Actions
+"""
 class DBV400(SerialDevice):
 
     def __init__(self, deviceManager):
@@ -18,6 +24,9 @@ class DBV400(SerialDevice):
     pass
 
 
+"""
+Class that maanages our Draxboard communication and state
+"""
 class Draxboard(SerialDevice):
 
     def __init__(self, deviceManager):
@@ -25,6 +34,11 @@ class Draxboard(SerialDevice):
 
     pass
 
+"""
+Supplimentary class to our reliance printers. This class talks to the reliance printer through
+serial communication to retrieve the state of the device and the paper level. Handles other printer
+commands that are special to the reliance printer as well
+"""
 class ReliancePrinterSerial(SerialDevice):
     def __init__(self, deviceManager):
 
@@ -32,6 +46,11 @@ class ReliancePrinterSerial(SerialDevice):
 
     pass
 
+"""
+Class that handles communication with our connected omnidongle. Unlike other classes that handle the state and functionality of
+our device, this class strictly relays packets between this device and our Unity Application. Only state we are concerned with
+in this class is whether or not it is connected
+"""
 class Omnidongle(SerialDevice):
     def __init__(self, deviceManager):
 
