38b3db31a4f96eba9ed2c9f3de16a00c0d4e4f2c
Ryan Andersen <ryan.andersen05@gmail.com>
2019-09-21 20:11:59 -0400

    Added exception handling for printer state thread

--- DragonMasterDevice.py
+++ DragonMasterDevice.py
@@ -226,6 +226,9 @@ class Printer(DragonMasterDevice):
                 self.currentState = self.get_updated_printer_state_and_paper_state()
                 if self.currentState != self.lastSentPrinterState:
                     self.dragonMasterDeviceManager.add_event_to_send(DragonMasterDeviceManager.DragonMasterDeviceManager.PRINTER_STATE_EVENT, [self.currentState[0], self.currentState[1]], self.get_player_station_hash())
+            except Exception as e:
+                print ("There was a problem getting the state of the printer")
+                print (e)
             sleep (.5)
 
     """
