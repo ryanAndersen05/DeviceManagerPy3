04b0b0258622eb21385212a1a7defe9af5b463bf
Ryan Andersen <ryan.andersen05@gmail.com>
2020-03-18 19:19:59 -0400

    Adding CFFT to our project

--- Scripts/BookkeepingManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BookkeepingManager : MonoBehaviour {
-
-	public const string LinuxPersistentDirectory = "/opt/unity3d/";
-}
